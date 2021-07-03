1e1392fe935f50419b4650265119edfb91aead2e
Lynda Sowunmi <lyndasownmi@yahoo.com>
2021-07-03 00:13:12 -0500

    Add editTodo function

--- src/App.js
+++ src/App.js
@@ -38,6 +38,19 @@ function App() {
     setTodos(updatedTodos);
   }
 
+
+  function editTodo(id) {
+    const updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.text = editingText;
+      }
+      return todo;
+    })
+    setTodos(updatedTodos);
+    setTodoEditing(null);
+    setEditingText("");
+  }
+
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
@@ -46,8 +59,12 @@ function App() {
       </form>
       {todos.map((todo) => <div key={todo.id}>
 
-        <div>{todo.text}</div>
-        <input type="text" onChange={(e) => setEditingText(e.target.value)} value={editingText} />
+        {todoEditing === todo.id ?
+          (<input type="text" onChange={(e) => setEditingText(e.target.value)} value={editingText} />)
+          :
+          (<div>{todo.text}</div>)}
+
+
 
         <button onClick={() => deleteTodo(todo.id)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
           <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
@@ -57,6 +74,8 @@ function App() {
         <button onClick={() => setTodoEditing(todo.id)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
           <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
         </svg></button>
+        <button onClick={() => editTodo(todo.id)}>Submit Edits</button>
+
       </div>)}
     </div >
   );
