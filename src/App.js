a8440762f9d21a37495262cd7ded388cf74e4e9d
Lynda Sowunmi <lyndasownmi@yahoo.com>
2021-07-02 23:02:35 -0500

    Add handlesubmit function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  const [todos, setTodo] = React.useState([])
-  const [todo, setTodo] = React.useState("")
+  const [todos, setTodos] = React.useState([]);
+  const [todo, setTodo] = React.useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      completed: false,
+    }
+
+    setTodos([...todos].concat(newTodo))
+    setTodo("")
+  }
 
   return (
     <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo} />
+        <button type="submit">Add To List</button>
+      </form>
+      {todos.map((todo) => <div key={todo.id}>
+        <div>{todo.text}</div>
+        <button onClick={() => deletTodo(todo.id)}>Delete</button>
 
+      </div>)}
     </div>
   );
 }
