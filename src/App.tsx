ddff5e906569446dae311c0ba1c5d3a6e56fa9fa
Lynda Sowunmi <lyndasownmi@yahoo.com>
2021-08-07 22:02:55 -0500

    Add login/logout authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,41 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Userfront from "@userfront/react";
+import { Redirect} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css'; 
 import ToDo  from "./ToDo";
+
 import './App.css';
 
 export const addTodo = (todos, newTodo) => [...todos].concat(newTodo) /*  JEST testing the handleSubmit("addToDo") function */
 export const deleteTodo = (todos, deletedToDo) => [...todos.slice(0, deletedToDo)] /*  JEST testing the handleSubmit("deleteToDo") function */
 
 
-Userfront.init("demo1234");
+Userfront.init("7n84y9n6");
 
 const SignupForm = Userfront.build({
-  toolId: "nkmbbm",
+  toolId: "ldmlao",
 });
 const LoginForm = Userfront.build({
-  toolId: "alnkkd",
+  toolId: "bmbano",
+})
+//const PasswordResetForm = Userfront.build({
+ // toolId: "nabkna",
+//});
+const LogoutButton = Userfront.build({
+  toolId: "klodan",
 });
-const PasswordResetForm = Userfront.build({
-  toolId: "dkbmmo",
-});
+
 
 
 export default function App() {
   return (
     <Router>
       <div>
+          
+            <span >
+              <Link to="/"><img src="https://img.icons8.com/cute-clipart/64/000000/reminders.png" alt="" /> <b className="logo-text">The ToDo Organizer</b></Link>
+            </span>
+            
         <nav id="nav-list">
           <ul>
             <li>
@@ -31,13 +43,7 @@ export default function App() {
             </li>
             <li>
               <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/reset">Reset</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
+            </li> 
           </ul>
         </nav>
 
@@ -45,11 +51,11 @@ export default function App() {
           <Route path="/login">
             <Login />
           </Route>
-          <Route path="/reset">
-            <PasswordReset />
-          </Route>
           <Route path="/dashboard">
             <Dashboard />
+          </Route> 
+          <Route path="/logout" > 
+          <LogOut/>
           </Route>
           <Route path="/">
             <Home />
@@ -76,16 +82,53 @@ function Login() {
   );
 }
 
-function PasswordReset() {
+/*function PasswordReset() {
   return (
     <div>
       <PasswordResetForm />
     </div>
   );
+} */
+
+function LogOut() {
+  return (
+    <div>
+     <LoginForm/>
+    </div>
+  ); 
 }
 
-function Dashboard() {
+/*function Dashboard() {
   return (
-   <ToDo />
+    <div> <button type="submit" onClick={LogOut} ><Link to="/login">Log out</Link></button><div>
+
+    </div>< ToDo /> </div>
   );
+} */
+
+function Dashboard() {
+  function renderFn({ location }) {
+    // If the user is not logged in, redirect to login
+    if (!Userfront.accessToken()) {
+      return (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: location },
+          }}
+        />
+      );
+    }
+
+    // If the user is logged in, show the dashboard
+    const userData = JSON.stringify(Userfront.user, null, 2);
+    return (
+          <div > <button type="button" id="log-out-button" className="btn btn-danger" onClick={Userfront.logout} ><Link  id="log-out-button" to="/login">Log out</Link></button><div>
+          </div>< ToDo /> </div>
+        
+      
+    );
+  }
+
+  return <Route render={renderFn} />;
 }
--- src/ToDo.tsx
+++ src/ToDo.tsx
@@ -55,17 +55,33 @@ function ToDo() {
   }
 
   /* Edit Task Function */
+
   function editTodo(id) {
     const updatedTodos = [...todos].map((todo) => {
       if (todo.id === id) {
+        console.log(id);
         todo.text = editingText;
       }
       return todo;
     })
     setTodos(updatedTodos);
+    
     setTodoEditing(null); /* resets editing */
+    console.log(todo,id);
     setEditingText(""); /* resets editing */
   }
+
+/* Completed strikethrough*/
+
+ function crossLine(event) {
+      const element = event.target;
+      element.classList.toggle("crossed-line");
+  };
+
+  
+  
+
+  
 
   return (
     <div id="todo-list">
@@ -79,11 +95,12 @@ function ToDo() {
       </form>
 
       {todos.map((todo) => <div key={todo.id} className="todo">
-      <input type="checkbox" onChange={() => toggleCompleted(todo.id)} checked={todo.completed} />
+      <input type="checkbox"  onChange={() => toggleCompleted(todo.id)}  checked={todo.completed} /> 
+
         {todoEditing === todo.id ?
           (<input id="todo-edits" type="text" placeholder="Edit task..." onChange={(e) => setEditingText(e.target.value)} value={editingText} />)
           :
-          (<div className="todo-text">{todo.text}</div>)}
+          (<div className="todo-text">{todo.text}</div>)} 
 
         <div>
           <button className="todo-actions" onClick={() => deleteTodo(todo.id)}>
@@ -93,16 +110,20 @@ function ToDo() {
           </svg>
           </button>
 
-          {todoEditing === todo.id ? /* shows one button at a time instead of both depending on the action needed */
+          
+
+          {todoEditing === todo.id ?
+           /* shows one button at a time instead of both depending on the action needed */
             (<button id="todo-save" onClick={() => editTodo(todo.id)}>Save</button>)
             :
             (<button className="todo-actions" onClick={() => setTodoEditing(todo.id)}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pen" viewBox="0 0 16 16">
               <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
             </svg>
-            </button>)}
+            </button>
+             )}
+    
         </div>
-
       </div>)}
     </div>
   );
